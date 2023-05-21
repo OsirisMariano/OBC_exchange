b89a86b9955153516cc7270b50a9e148fcb97c04
Osiris Mariano <oslimacontato@gmail.com>
2023-05-21 20:20:55 -0300

    Aula 11: Edita arquivos controller & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'exchanges#index'
-  get 'convert', to 'exchange#convert'
+  get 'convert', to: 'exchange#convert'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
